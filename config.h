f89c8c353cab37afdb59cdc91fef2f5b632815ac
Kristin Ottofy <kottofy@outlook.com>
2017-08-07 10:00:37 -0400

    initial commit

---
+++ config.h
@@ -0,0 +1,27 @@
+// Physical device information for board and sensor
+#define DEVICE_ID "Feather HUZZAH ESP8266 WiFi"
+
+// Pin layout configuration
+#define LED_PIN LED_BUILTIN
+
+// Interval time(ms) for sending message to IoT Hub
+#define INTERVAL 2000
+
+// EEPROM address configuration
+#define EEPROM_SIZE 512
+
+// SSID and SSID password's length should < 32 bytes
+// http://serverfault.com/a/45509
+#define SSID_LEN 32
+#define PASS_LEN 32
+#define CONNECTION_STRING_LEN 256
+
+#define MESSAGE_MAX_LEN 256
+
+
+#define CONNECTION_STRING  "YOUR_DEVICE_CONNECTION_STRING_HERE"
+
+#define WIFI_SSID     "YOUR_WIFI_SSID_HERE"
+#define WIFI_PASS     "YOUR_WIFI_PASSWORD_HERE"
+
+
